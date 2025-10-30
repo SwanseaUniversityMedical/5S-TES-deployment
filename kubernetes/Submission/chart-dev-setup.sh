63425d3a71356e43264e1c6e94022b11b96907ac
Alex <a.s.lee@swansea.ac.uk>
2025-10-30 11:15:01 +0000

    feat: add example local kubernetes deployment of submission layer

---
+++ kubernetes/Submission/chart-dev-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# create kind cluster with config
+kind create cluster --config=files/kind-config.yaml
+
+# install ingress-nginx
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace -f files/ingress-nginx.yaml
+
+# rewrite coredns
+kubectl apply -f files/coredns.yaml
+
+# install argo
+helm repo add argo https://argoproj.github.io/argo-helm 
+helm repo update
+helm upgrade --install argocd argo/argo-cd -n argocd -f files/argo.yaml --create-namespace
+
+# add repo and project
+kubectl apply -f files/project.yaml
+kubectl apply -f files/repo.yaml
+kubectl create ns dare-control-submission
+kubectl apply -f files/keycloak.yaml
+
+# get the argo password and emit it
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+./files/deps.sh
