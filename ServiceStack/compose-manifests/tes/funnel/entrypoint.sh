1190074fc4bed292a0e53b3fa1ebe217397d4e0b
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2026-02-27 15:52:28 +0000

    Remove print from funnel entrypoint

--- ServiceStack/compose-manifests/tes/funnel/entrypoint.sh
+++ ServiceStack/compose-manifests/tes/funnel/entrypoint.sh
@@ -3,7 +3,5 @@ sed \
   -e "s|\${MinioKey}|$MinioKey|g" \
   -e "s|\${MinioSecret}|$MinioSecret|g" \
   config/config.template.yaml > config.yaml
-echo "##### ----- Funnel config start -----"
-cat config.yaml
-echo "##### ----- Funnel config end -----"
+
 exec funnel server run --config config.yaml
