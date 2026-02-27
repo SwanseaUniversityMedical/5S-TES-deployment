492d41c62792524ae7a32d73d59ee7d5fbe9e3eb
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2026-02-27 15:21:15 +0000

    Print funnel config on startup (for debugging)

--- ServiceStack/compose-manifests/tes/funnel/entrypoint.sh
+++ ServiceStack/compose-manifests/tes/funnel/entrypoint.sh
@@ -3,4 +3,7 @@ sed \
   -e "s|\${MinioKey}|$MinioKey|g" \
   -e "s|\${MinioSecret}|$MinioSecret|g" \
   config/config.template.yaml > config.yaml
+echo "##### ----- Funnel config start -----"
+cat config.yaml
+echo "##### ----- Funnel config end -----"
 exec /root/.local/bin/funnel server run --config config.yaml
